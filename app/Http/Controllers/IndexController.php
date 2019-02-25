f29bd308ca9499e40465d87df4040a5717142f73
Viacheslav <zhizhkoslava@gmail.com>
2019-02-25 16:24:50 +0200

    test landing

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,11 +9,44 @@ use App\People;
 use App\Service;
 use App\Portfolio;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class IndexController extends Controller
 {
     //
     public function execute(Request $request) {
+
+        if ($request->isMethod('post')) {
+
+
+            $messages =[
+                'required' => "Поле :attribute обязательно к заполнению!",
+                'email' => "Поле :attribute должно соответствовать email адресу",
+            ];
+
+            $this->validate($request, [
+                'name' => 'required|max:255|',
+                'email' => 'required|email|',
+                'text' => 'required',
+            ], $messages);
+
+            $data = $request->all();
+
+            //mail
+            $result = Mail::send('site.email', ['data' => $data], function($message) use ($data) {
+
+                $mail_admin = env('MAIL_ADMIN');
+
+                $message->from($data['email'], $data['name']);
+                $message->to($mail_admin)->subject('Question');
+
+            });
+
+            if ($result) {
+                return redirect()->route('home')->with('status', 'Email is sent');
+            }
+
+        }
 
         $pages = Page::all();
         $portfolios = Portfolio::get(array('name', 'filter', 'images'));
--- resources/views/site/content.blade.php
+++ resources/views/site/content.blade.php
@@ -266,9 +266,9 @@
 
                         <form action="{{ route('home') }}" method="post">
 
-                        <input class="input-text" type="text" name="" value="Your Name *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
-                        <input class="input-text" type="text" name="" value="Your E-mail *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
-                        <textarea class="input-text text-area" cols="0" rows="0" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">Your Message *</textarea>
+                        <input class="input-text" type="text" name="name" value="Your Name *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
+                        <input class="input-text" type="text" name="email" value="Your E-mail *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
+                        <textarea name="text" class="input-text text-area" cols="0" rows="0" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">Your Message *</textarea>
                         <input class="input-btn" type="submit" value="send message">
 
                             @csrf
