b7125d60358aa55b348f4a3cd3dbe4dd2588159e
Viacheslav <zhizhkoslava@gmail.com>
2019-02-28 14:58:25 +0200

    add pages and pagesAdd

---
+++ resources/views/admin/content_pages.blade.php
@@ -0,0 +1,47 @@
+<div style="margin: 0px 50px 0px 50px;">
+
+    @if(isset($pages))
+
+        <table class="table table-hover table-striped">
+            <thead>
+                <tr>
+                    <th>№ п/п</th>
+                    <th>Имя</th>
+                    <th>Псевдоним</th>
+                    <th>Текст</th>
+                    <th>Удалить</th>
+                </tr>
+            </thead>
+
+
+            <tbody>
+
+            @foreach($pages as $k => $page)
+
+                <tr>
+
+                    <td>{{  $page->id }}</td>
+                    <td>{!! Html::link(route('pagesEdit', ['page' => $page->id]), $page->name, ['alt' => $page->name]) !!}</td>
+                    <td>{{  $page->alias }}</td>
+                    <td>{{  $page->text }}</td>
+                    <td>{{  $page->created_at }}</td>
+                    <td>
+                        {!! Form::open(['url' => route('pagesEdit', ['page' => $page->id]), 'class' => 'form-horizontal', 'method' => 'POST']) !!}
+
+                            {!! Form::hidden('action', 'delete') !!}
+                            {!! Form::button('Удалить', ['class' => 'btn btn-danger', 'type' => 'submit']) !!}
+
+                        {!! Form::close() !!}
+                    </td>
+                </tr>
+            @endforeach
+
+            </tbody>
+
+        </table>
+
+    @endif
+
+        {!! Html::link(route('pagesAdd'), 'Новая страница') !!}
+
+</div>
